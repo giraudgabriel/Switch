685d31f49651342ad10260b2e223916230b5c518
Gabriel Giraud <gabriel@multicontas.com.br>
2019-08-03 18:03:26 -0300

    #FEATURE: Migrations da tabela Postagens.

---
+++ Switch.Infra.Data/Migrations/20190803204250_CriarTabelaPostagem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class CriarTabelaPostagem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Postagens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DataPublicacao = table.Column<DateTime>(nullable: false),
+                    Texto = table.Column<string>(maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Postagens", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Postagens");
+        }
+    }
+}
